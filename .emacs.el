cab7b120b407d00a96b63c6db16edddf1e01dc43
Yusuke Obayashi <grandeforesta@gmail.com>
2009-05-21 07:40:58 +0900

    sense-regionの設定を追加

--- .emacs.el
+++ .emacs.el
@@ -1,44 +1,48 @@
-    (defun fullscreen ()
-     (interactive)
-     (set-frame-parameter nil 'fullscreen
-      (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
-    (global-set-key "\C-cm" 'fullscreen)
+(defun fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen
+					   (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
+(global-set-key "\C-cm" 'fullscreen)
 
-    ;; Color
-    (if window-system (progn
-                       (set-background-color "Black")
-                       (set-foreground-color "LightGray")
-                       (set-cursor-color "Gray")
-                       (set-frame-parameter nil 'alpha 80)
-                      ))
+;; Color
+(if window-system (progn
+					(set-background-color "Black")
+					(set-foreground-color "LightGray")
+					(set-cursor-color "Gray")
+					(set-frame-parameter nil 'alpha 80)
+					))
 
-    ;; Macのキーバインドを使う。optionをメタキーにする。
+;; Macのキーバインドを使う。optionをメタキーにする。
 (mac-key-mode 1)
-    (setq mac-option-modifier 'meta)
+(setq mac-option-modifier 'meta)
 
-    ;; タブキー
-    (setq default-tab-width 4)
-    (setq indent-line-function 'indent-relative-maybe)
+;; タブキー
+(setq default-tab-width 4)
+(setq indent-line-function 'indent-relative-maybe)
      
-     ;; シフト + 矢印で範囲選択
-     (setq pc-select-selection-keys-only t)
-     (pc-selection-mode 1)
+;; シフト + 矢印で範囲選択
+(setq pc-select-selection-keys-only t)
+(pc-selection-mode 1)
       
-      ;; 行数を表示
-       (setq line-number-mode t)
+;; 行数を表示
+(setq line-number-mode t)
         
-        ;; ビープ音を消す
-        (setq visible-bell t)
-        (setq ring-bell-function 'ignore)
+;; ビープ音を消す
+(setq visible-bell t)
+(setq ring-bell-function 'ignore)
          
-         ;; スタートアップページを表示しない
-         (setq inhibit-startup-message t)
+;; スタートアップページを表示しない
+(setq inhibit-startup-message t)
           
-          ;; 対応する括弧を光らせる。
-          (show-paren-mode 1)
+;; 対応する括弧を光らせる。
+(show-paren-mode 1)
            
-           ;; 編集行のハイライト
-           (global-hl-line-mode)
+;; 編集行のハイライト
+(global-hl-line-mode)
             
-            ;; C-hをBSに割当
-            (global-set-key "\C-h" 'delete-backward-char)
+;; C-hをBSに割当
+(global-set-key "\C-h" 'delete-backward-char)
+
+(setq load-path (cons (expand-file-name "~/elisp") load-path))
+(autoload 'sense-region-on "sense-region.el"
+          "System to toggle region and rectangle." t nil)
