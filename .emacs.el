5c3d944a429d1cccfd38b3c4b247e7bfb3a43e73
Yusuke Obayashi <grandeforesta@gmail.com>
2009-05-21 23:41:54 +0900

    session minibuf-isearch追加

--- .emacs.el
+++ .emacs.el
@@ -46,3 +46,19 @@
 (setq load-path (cons (expand-file-name "~/elisp") load-path))
 (autoload 'sense-region-on "sense-region.el"
           "System to toggle region and rectangle." t nil)
+
+;; ミニバッファ履歴リストの最大長：tなら無限
+(setq history-length t)
+;; session.el
+;;   kill-ringやミニバッファで過去に開いたファイルなどの履歴を保存する
+(when (require 'session nil t)
+  (setq session-initialize '(de-saveplace session keys menus places)
+        session-globals-include '((kill-ring 50)
+                                  (session-file-alist 500 t)
+                                  (file-name-history 10000)))
+  (add-hook 'after-init-hook 'session-initialize)
+  ;; 前回閉じたときの位置にカーソルを復帰
+  (setq session-undo-check -1))
+;; minibuf-isearch
+;;   minibufでisearchを使えるようにする
+(require 'minibuf-isearch nil t)
